e753aea0ab67f894eecc0c6ee815cf6e8f54d6f8
Elmar Athmer <elmar@athmer.org>
2014-04-21 18:09:03 +0200

    stub CaesarCipher in F#

---
+++ Caesar.F/CaesarCipher.fs
@@ -0,0 +1,15 @@
+﻿namespace Caesar.F
+
+type CaesarCipher(a: int, b: int) =
+    member this.A = a
+    member this.B = b
+    new(cipherText: string) = CaesarCipher(1, 2)
+
+    static member CharToRingElement(x: char): int = 0
+    static member RingElementToChar(x: int): char = 'a'
+    
+    member this.Decrypt(c: string) = c
+    member this.Decrypt(c: char) = c
+
+    member this.Encrypt(m: char) = m
+    member this.Encrypt(m: string) = m
---
+++ CaesarTest/FCaesarCipherTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Caesar.F;
+
+namespace CaesarTest
+{
+    [TestClass]
+    public class FCaesarChiperTest
+    {
+        [TestMethod]
+        public void CharToRingElement()
+        {
+            Assert.AreEqual(CaesarCipher.CharToRingElement('a'), 0);
+            Assert.AreEqual(CaesarCipher.CharToRingElement('z'), 25);
+            Assert.AreEqual(CaesarCipher.CharToRingElement('e'), 4);
+        }
+
+        public void RingElementToChar()
+        {
+            Assert.AreEqual(CaesarCipher.RingElementToChar(0), 'a');
+            Assert.AreEqual(CaesarCipher.RingElementToChar(25), 'z');
+            Assert.AreEqual(CaesarCipher.RingElementToChar(4), 'e');
+        }
+
+        [TestMethod]
+        public void Crack()
+        {
+            var crackedSystem = new CaesarCipher("CKDCSQDLQJEWJGFQIBQIIQDUQJJQJCQJJCKDQIJKGFNAWDRSAQJWSQDUQJJQJCOZZNQJ");
+            Assert.AreEqual(5, crackedSystem.A);
+            Assert.AreEqual(22, crackedSystem.B);
+        }
+
+        [TestMethod]
+        public void EncryptChars()
+        {
+            var subject = new CaesarCipher(5, 22);
+            Assert.AreEqual('c', subject.Encrypt('w'));
+        }
+
+        [TestMethod]
+        public void EncryptStrings()
+        {
+            var subject = new CaesarCipher(5, 22);
+            Assert.AreEqual("cozznqj", subject.Encrypt("wollten"));
+            Assert.AreEqual("cozznqj", subject.Encrypt("WOLLTEN"));
+        }
+
+        [TestMethod]
+        public void DecryptStrings()
+        {
+            var subject = new CaesarCipher(5, 22);
+            Assert.AreEqual("wollten", subject.Decrypt("cozznqj"));
+        }
+    }
+}
