16d2ede28e83c7173a06db37ee365f11c45cc697
Elmar Athmer <elmar@athmer.org>
2014-04-21 17:24:47 +0200

    add f# implementation for ModuloArithmetic

---
+++ Caesar.F/ModuloArithmetic.fs
@@ -0,0 +1,19 @@
+﻿namespace Caesar.F
+
+type ModuloArithmetic(modulo: int) = 
+    static member Egcd(a: int, b: int) : int * int * int =
+        if b = 0 then (a, 1, 0)
+        else 
+            let (d', s', t') = ModuloArithmetic.Egcd(b, a % b)
+            (d', t', s' - (a / b) * t')
+        
+    member this.Congruent(v: int) =
+        if v < 0 then this.Congruent(v + modulo);
+        else v % modulo
+
+    member this.Inverse(x: int) = 
+        if x < 0 then this.Inverse(this.Congruent(x))
+        else
+            let (ggt, t, s) = ModuloArithmetic.Egcd(modulo, x)
+            if ggt <> 1 then raise (System.ArgumentException("ggt != 1"))
+            else s
---
+++ CaesarTest/FModuloArithmeticTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Caesar.F;
+
+namespace CaesarTest
+{
+    [TestClass]
+    public class FModuloArithmeticTest
+    {
+        ModuloArithmetic subject;
+        
+        [TestInitialize]
+        public void SetUp()
+        {
+            subject = new ModuloArithmetic(26);
+        }
+
+        [TestMethod]
+        public void Egcd()
+        {
+            Assert.AreEqual(Tuple.Create(1, 1, -2), ModuloArithmetic.Egcd(27, 13));
+            Assert.AreEqual(Tuple.Create(1, 11, -6), ModuloArithmetic.Egcd(23, 42));
+        }
+
+        [TestMethod]
+        public void Inverse()
+        {
+            Assert.AreEqual(subject.Inverse(9), 3);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void NoInverse()
+        {
+            subject.Inverse(91);
+        }
+
+        [TestMethod]
+        public void Congruent()
+        {
+            Assert.AreEqual(6, subject.Congruent(-20));
+            Assert.AreEqual(6, subject.Congruent(32));
+            Assert.AreEqual(25, subject.Congruent(-261));
+        }
+    }
+}
